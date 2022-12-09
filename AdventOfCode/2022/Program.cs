9b0ea8bbfb3922be3689cf0af8d1c8b64f0638a2
arajgor2000 <98547229+arajgor2000@users.noreply.github.com>
2022-12-09 23:17:41 +0000

    Setup of solution

---
+++ AdventOfCode/2022/Program.cs
@@ -0,0 +1,10 @@
+﻿using AdventOfCode.Framework;
+using AdventOfCode.Framework.Runner;
+
+SolutionRunner runner = new( 
+    new Options()
+    {
+        InputBaseDirectory = "Inputs"
+    });
+
+runner.Solve(1, Problem.Problem1);
---
+++ AdventOfCode/2022/Solutions/Day1.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode.Framework;
+
+namespace _2022.Solutions;
+
+[Solution(1)]
+[SolutionInput("Day1.txt")]
+public class Day1 : Solution
+{
+    public Day1(Input input) : base(input)
+    {
+    }
+
+    protected override string? Problem1()
+    {
+        return null;
+    }
+
+    protected override string? Problem2()
+    {
+        return null;
+    }
+}
