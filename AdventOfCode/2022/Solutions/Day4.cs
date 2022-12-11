b019e76a331dd2ae1f676b2c27a3c59b3da896a5
arajgor2000 <98547229+arajgor2000@users.noreply.github.com>
2022-12-11 21:02:48 +0000

    Day 4 - problem 1

--- AdventOfCode/2022/Solutions/Day4.cs
+++ AdventOfCode/2022/Solutions/Day4.cs
@@ -15,7 +15,27 @@ public class Day4 : Solution
 
     protected override string? Problem1()
     {
-        return null;
+        long sum = 0;
+        foreach (var line in _lines)
+        {
+            var ranges = line.Split(',');
+            
+            var range1Limits = ranges[0].Split('-');
+            var range1Lower = int.Parse(range1Limits[0]);
+            var range1Upper = int.Parse(range1Limits[1]);
+
+            var range2Limits = ranges[1].Split('-');
+            var range2Lower = int.Parse(range2Limits[0]);
+            var range2Upper = int.Parse(range2Limits[1]);
+
+            if ((range1Lower <= range2Lower && range1Upper >= range2Upper) || 
+                (range2Lower <= range1Lower && range2Upper >= range1Upper))
+            {
+                sum++;
+            }
+        }
+
+        return sum.ToString();
     }
 
     protected override string? Problem2()
