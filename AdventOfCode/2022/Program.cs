9ff5ad463f529def0404edb833183ddc143ba1e9
arajgor2000 <98547229+arajgor2000@users.noreply.github.com>
2022-12-11 20:45:38 +0000

    Day 4 template

--- AdventOfCode/2022/Program.cs
+++ AdventOfCode/2022/Program.cs
@@ -7,4 +7,4 @@ SolutionRunner runner = new(
         InputBaseDirectory = "Inputs"
     });
 
-runner.Solve(3, Problem.All);
+runner.Solve(4, Problem.All);
---
+++ AdventOfCode/2022/Solutions/Day4.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode.Framework;
+
+namespace _2022.Solutions;
+
+[Solution(4)]
+[SolutionInput("Day4.txt")]
+public class Day4 : Solution
+{
+    private readonly string[] _lines;
+
+    public Day4(Input input) : base(input)
+    {
+        _lines = input.Lines;
+    }
+
+    protected override string? Problem1()
+    {
+        return null;
+    }
+
+    protected override string? Problem2()
+    {
+        return null;
+    }
+}
